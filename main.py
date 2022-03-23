b04fd8e99dbe670f4b05f5c465a5370393907f72
IgorR02 <igor.ropiak@gmail.com>
2022-03-23 10:52:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import random
+import sys
+#Zad1
+# list = []
+# for x in range(0,31):
+#     list.append(x*2)
+# file = open("data.txt", "w+")
+# file.write(str(list))
+# file.close()
+
+#Zad2
+# file = open("data.txt")
+# num = file.readline()
+# file.close()
+# print(num)
+
+#Zad3
+# file = open("text.txt", "a+")
+# lines = "Kilka linijek \n przykladowego tekstu"
+# file.write(lines)
+# file.close()
+# with open("text.txt") as file:
+#         for line in file:
+#                 print(line, end = "")
+
+#Zad4
+# class LetsShop:
+#         """Klasa na zakupy"""
+#         def __init__(self,product_name, quantity,unit_of_measure, unite_price):
+#                 self.product_name = product_name
+#                 self.quantity = quantity
+#                 self.unit_of_measure = unit_of_measure
+#                 self.unite_price = unite_price
+#                 self.value = self.quantity * self.unite_price
+#
+#         def printProduct(self):
+#             print(self.product_name,"cost = ", str(self.value))
+#
+#         def howMany(self):
+#             print(str(self.quantity), str(self.unit_of_measure))
+#
+#         def cost(self):
+#             print(str(self.unite_price),"per",str(self.unit_of_measure))
+#
+# milk = LetsShop("Milk", 1, "l", 3.99)
+# apple = LetsShop("Apple", 2, "kg", 1.99)
+# apple.printProduct()
+# apple.howMany()
+# apple.cost()
+# milk.printProduct()
+# milk.howMany()
+# milk.cost()
+
+#Zad5
+# class ArithmeticString:
+#     def __init__(self, a1, difference, length):
+#         self.a1 = a1
+#         self.differnce = difference
+#         self.length = length
+
+#Zad6
+# class Warm:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+
+
